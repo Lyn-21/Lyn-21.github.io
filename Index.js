7302e301c3b77a9e393e0c057142ec3d13cda327
Lyn-21 <gonzalesevangelyn22@gmail.com>
2024-05-25 11:39:38 +0800

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,1 +1,10 @@
+const btnlike1 = document.getElementById("btnlike1")
+const btnlike1 = document.getElementById("btnlike2")
+const countlikes1 = document.getElementById("countlike1")                                       
+const countlikes2 = document.getElementById("countlike2")                                       
 
+function hitlike1() {
+  let nolike1 = parsent(countlike1.value) + 1
+  countlike1.textContent = totallike1.toString()
+}
+btnlike1.addEventListener("click,hitlikes1()
